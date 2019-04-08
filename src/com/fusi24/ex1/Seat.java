30ffb7a326492e425bdf816e21adff080a928d9d
Medisa Aris <medisa.aris@fusi24.com>
2019-04-08 16:13:40 +0700

    create class seat

---
+++ src/com/fusi24/ex1/Seat.java
@@ -0,0 +1,25 @@
+package com.fusi24.ex1;
+
+public class Seat {
+	private String seatName;
+	
+	public boolean addSeat() {
+		return true;
+	}
+	
+	public boolean editSeat() {
+		return true;
+	}
+	
+	public boolean removeSeat() {
+		return true;
+	}
+	
+	public String getSeat() {
+		return seatName;
+	}
+	
+	public void setSeat(String seatName) {
+		this.seatName = seatName;
+	}
+}
