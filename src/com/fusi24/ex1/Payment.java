d90edb08859e24f0c07002d7d0eb3e5762b2fcc4
yusup <yusup@DESKTOP-FF2T7EV>
2019-04-08 16:46:32 +0700

    Payment

---
+++ src/com/fusi24/ex1/Payment.java
@@ -0,0 +1,32 @@
+package com.fusi24.ex1;
+
+
+public class Payment {
+	private String invoiceNumber;
+	
+	private String paymentName;
+	
+	
+	public boolean addPayment() {
+		return true;
+	}
+	
+	public boolean editPayment() {
+		return true;
+	}
+	
+	public boolean removePayment() {
+		return true;
+	}
+
+	public String getInvoiceNumber() {
+		return invoiceNumber;
+	}
+
+	public void setInvoiceNumber(String invoiceNumber) {
+		this.invoiceNumber = invoiceNumber;
+	}
+	
+
+	
+}
