178e38c80aff26afad318e63edf220f04268967b
Asus <endah.aulia26@gmail.com>
2019-04-08 16:25:28 +0700

    membuat class venue

---
+++ src/com/fusi24/ex1/Venue.java
@@ -0,0 +1,35 @@
+package com.fusi24.ex1;
+
+public class Venue {
+	private String venueName;
+	private String venueLocation;
+	
+	public boolean addVenue() {
+		return true;
+	}
+	
+	public boolean editVenue() {
+		return true;
+	}
+	
+	public boolean removeVenue() {
+		return true;
+	}
+
+	public String getVenueName() {
+		return venueName;
+	}
+
+	public void setVenueName(String venueName) {
+		this.venueName = venueName;
+	}
+
+	public String getVenueLocation() {
+		return venueLocation;
+	}
+
+	public void setVenueLocation(String venueLocation) {
+		this.venueLocation = venueLocation;
+	}
+	
+}
